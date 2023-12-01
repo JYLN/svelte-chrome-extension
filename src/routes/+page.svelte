5bab6452292f5473a59f95b759422b5e61312f1d
JYLN <14062951+JYLN@users.noreply.github.com>
2023-12-01 11:02:02 -0700

    Built initial layout and logic for calculator functionality

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,28 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { evaluate } from 'mathjs';
+
+	let value: string = '';
+	let result: string = '';
+
+	function select(this: HTMLInputElement, event: Event) {
+		this.select();
+	}
+
+	function calculate() {
+		result = evaluate(value);
+	}
+</script>
+
+<svelte:head>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@1/css/pico.min.css" />
+	<link rel="stylesheet" href="popup.css" />
+</svelte:head>
+
+<div class="container">
+	<h1>Calculator</h1>
+	<form on:submit|preventDefault={calculate}>
+		<input type="text" placeholder="1 + 2" bind:value on:click={select} />
+	</form>
+
+	<p>{result}</p>
+</div>
